2578064efeb54f744748ea665e0c0391aefb7e9a
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2024-06-15 14:57:39 +0300

    Add documentation

--- build.zig
+++ build.zig
@@ -1,5 +1,24 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // Compile a library to allow building docs 
+    const natives_lib = b.addSharedLibrary(.{
+        .name = "natives",
+        .root_source_file = b.path("src/natives.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installDirectory(.{
+        .source_dir = natives_lib.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+    
+    // MODULE
     _ = b.addModule("shvz", .{ .root_source_file = b.path("src/shvz.zig") });
 }
